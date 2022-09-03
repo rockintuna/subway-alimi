b28154ed404d02191f8cf1b1ddb7ad2755ef8703
rockintuna <rockintuna@naver.com>
2022-09-03 20:52:17 +0900

    패키지 경로 변경

--- src/main/java/kr/co/e8ight/subwayalimi/subway/StationRepository.java
+++
@@ -1,6 +0,0 @@
-package kr.co.e8ight.subwayalimi.subway;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StationRepository extends JpaRepository<Station, Long> {
-}
--- src/main/java/kr/co/e8ight/subwayalimi/subway/TrainRepository.java
+++
@@ -1,6 +0,0 @@
-package kr.co.e8ight.subwayalimi.subway;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrainRepository extends JpaRepository<Train, Long> {
-}
